43df1ba4ac68a0ffb2e1b32c0bd74ded883c7421
Chris LaRose <cjlarose@gmail.com>
2020-11-30 21:38:13 -0800

    Complete part 1 of Day 1

--- src/Advent2020/Day01.hs
+++ src/Advent2020/Day01.hs
@@ -2,5 +2,32 @@ module Advent2020.Day01
   ( solve
   ) where
 
+import Text.Parsec (many1, sepEndBy1, eof)
+import Text.Parsec.Char (endOfLine, digit)
+import Text.Parsec.ByteString (Parser)
+
+import Advent.Input (getProblemInputAsByteString, withSuccessfulParse)
+
+entry :: Parser Int
+entry = read <$> many1 digit
+
+expenseReport :: Parser [Int]
+expenseReport = sepEndBy1 entry endOfLine <* eof
+
+pairs :: [Int] -> [(Int, Int)]
+pairs [] = []
+pairs (x:xs) = map (\y -> (x, y)) xs ++ pairs xs
+
+productOfSpecialEntries :: [Int] -> Int
+productOfSpecialEntries entries = x * y
+  where
+    (x, y) = head . filter (\(a, b) -> a + b == 2020) . pairs $ entries
+
+printResults :: [Int] -> (String, String)
+printResults entries = (part1, part2)
+  where
+    part1 = show . productOfSpecialEntries $ entries
+    part2 = "not yet implemented"
+
 solve :: IO (Either String (String, String))
-solve = pure . Right $ ("not yet implemented", "not yet implemented")
+solve = withSuccessfulParse expenseReport printResults <$> getProblemInputAsByteString 1
