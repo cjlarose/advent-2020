0d45cb705ec00ea5682afe249f85948f6729e62b
Chris LaRose <cjlarose@gmail.com>
2020-11-30 21:58:52 -0800

    Complete part 2 of Day 1

--- src/Advent2020/Day01.hs
+++ src/Advent2020/Day01.hs
@@ -18,16 +18,28 @@ pairs :: [Int] -> [(Int, Int)]
 pairs [] = []
 pairs (x:xs) = map (\y -> (x, y)) xs ++ pairs xs
 
-productOfSpecialEntries :: [Int] -> Int
-productOfSpecialEntries entries = x * y
+triples :: [Int] -> [(Int, Int, Int)]
+triples [] = []
+triples (x:xs) = withHead ++ withoutHead
+  where
+    withHead = map (\(y, z) -> (x, y, z)) . pairs $ xs
+    withoutHead = triples xs
+
+productOfSpecialPair :: [Int] -> Int
+productOfSpecialPair entries = x * y
   where
     (x, y) = head . filter (\(a, b) -> a + b == 2020) . pairs $ entries
+
+productOfSpecialTriple :: [Int] -> Int
+productOfSpecialTriple entries = x * y * z
+  where
+    (x, y, z) = head . filter (\(a, b, c) -> a + b + c == 2020) . triples $ entries
 
 printResults :: [Int] -> (String, String)
 printResults entries = (part1, part2)
   where
-    part1 = show . productOfSpecialEntries $ entries
-    part2 = "not yet implemented"
+    part1 = show . productOfSpecialPair $ entries
+    part2 = show . productOfSpecialTriple $ entries
 
 solve :: IO (Either String (String, String))
 solve = withSuccessfulParse expenseReport printResults <$> getProblemInputAsByteString 1
