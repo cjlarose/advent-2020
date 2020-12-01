f2add287988fd4e791db2aafa37d3450467d44c7
Chris LaRose <cjlarose@gmail.com>
2020-11-30 21:21:51 -0800

    Add Input module from last year

---
+++ src/Advent/Input.hs
@@ -0,0 +1,34 @@
+module Advent.Input
+  ( getProblemInputAsString
+  , getProblemInputAsByteString
+  , withSuccessfulParse
+  ) where
+
+import System.IO (hPutStrLn, stderr)
+import System.Exit (exitFailure)
+
+import qualified Data.ByteString as B
+import Text.Parsec (ParseError)
+import qualified Text.Parsec (parse)
+import Text.Parsec.ByteString (Parser)
+
+path :: Int -> String
+path problemNumber = "inputs/" ++ (show problemNumber) ++ ".txt"
+
+getProblemInputAsString :: Int -> IO String
+getProblemInputAsString = readFile . path
+
+getProblemInputAsByteString :: Int -> IO B.ByteString
+getProblemInputAsByteString = B.readFile . path
+
+handleParseError :: ParseError -> IO ()
+handleParseError err = do
+  hPutStrLn stderr "Parse error:"
+  hPutStrLn stderr . show $ err
+  exitFailure
+
+withSuccessfulParse :: Parser a -> (a -> (String, String)) -> B.ByteString -> Either String (String, String)
+withSuccessfulParse p f x = let res = Text.Parsec.parse p "" x
+  in case res of
+       Left err -> Left $ show err
+       Right parsed -> Right . f $ parsed
