89dba883d53dccaa572f749bdf4e63597cd743fd
Chris LaRose <cjlarose@gmail.com>
2020-12-01 21:52:49 -0800

    Use ADT for PasswordLine

--- src/Advent2020/Day02.hs
+++ src/Advent2020/Day02.hs
@@ -14,30 +14,30 @@ type Password = String
 data PasswordPolicy = PasswordPolicy { minOccurences :: Int
                                      , maxOccurences :: Int
                                      , character :: Char } deriving Show
-type PasswordLine = (PasswordPolicy, Password)
+data PasswordLine = PasswordLine PasswordPolicy Password
 
 passwordPolicy :: Parser PasswordPolicy
 passwordPolicy = PasswordPolicy <$> nonNegativeInteger <* char '-' <*> nonNegativeInteger <* char ' ' <*> lower
 
 passwordLine :: Parser PasswordLine
-passwordLine = (\a b -> (a, b)) <$> passwordPolicy <* string ": " <*> many1 lower
+passwordLine = PasswordLine <$> passwordPolicy <* string ": " <*> many1 lower
 
 passwordLines :: Parser [PasswordLine]
 passwordLines = linesOf passwordLine
 
-checkValid :: PasswordPolicy -> Password -> Bool
-checkValid (PasswordPolicy minOccurences maxOccurences character) password = occurences <= maxOccurences && occurences >= minOccurences
+checkValid :: PasswordLine -> Bool
+checkValid (PasswordLine (PasswordPolicy minOccurences maxOccurences character) password) = occurences <= maxOccurences && occurences >= minOccurences
   where
     occurences = length . filter (character ==) $ password
 
-checkValidPositions :: PasswordPolicy -> Password -> Bool
-checkValidPositions (PasswordPolicy i j character) = (1 ==) . length . filter id . zipWith (\index c -> c == character && (index == i || index == j)) [1..]
+checkValidPositions :: PasswordLine -> Bool
+checkValidPositions (PasswordLine (PasswordPolicy i j character) password) = (1 ==) . length . filter id . zipWith (\index c -> c == character && (index == i || index == j)) [1..] $ password
 
 printResults :: [PasswordLine] -> PuzzleAnswerPair
 printResults lines = PuzzleAnswerPair (part1, part2)
   where
-    part1 = show . length . filter (uncurry checkValid) $ lines
-    part2 = show . length . filter (uncurry checkValidPositions) $ lines
+    part1 = show . length . filter checkValid $ lines
+    part2 = show . length . filter checkValidPositions $ lines
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = withSuccessfulParse passwordLines printResults <$> getProblemInputAsByteString 2
