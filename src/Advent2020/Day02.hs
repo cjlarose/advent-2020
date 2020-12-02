24d87c5cd3ee55608928cf225eb1320b85358984
Chris LaRose <cjlarose@gmail.com>
2020-12-01 21:37:16 -0800

    Complete part 2 of Day 2

--- src/Advent2020/Day02.hs
+++ src/Advent2020/Day02.hs
@@ -30,11 +30,14 @@ checkValid (PasswordPolicy minOccurences maxOccurences character) password = occ
   where
     occurences = length . filter (character ==) $ password
 
+checkValidPositions :: PasswordPolicy -> Password -> Bool
+checkValidPositions (PasswordPolicy i j character) = (1 ==) . length . filter id . zipWith (\index c -> c == character && (index == i || index == j)) [1..]
+
 printResults :: [PasswordLine] -> PuzzleAnswerPair
 printResults lines = PuzzleAnswerPair (part1, part2)
   where
     part1 = show . length . filter (uncurry checkValid) $ lines
-    part2 = "not yet implemented"
+    part2 = show . length . filter (uncurry checkValidPositions) $ lines
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = withSuccessfulParse passwordLines printResults <$> getProblemInputAsByteString 2
