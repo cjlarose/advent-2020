b9f31b356792608a83974ca2a3c499487e9c8272
Chris LaRose <cjlarose@gmail.com>
2020-12-02 22:51:36 -0800

    Inline numTreesOnSlope

--- src/Advent2020/Day03.hs
+++ src/Advent2020/Day03.hs
@@ -21,18 +21,14 @@ treeMap = linesOf mapLine
     mapLine = cycle <$> many1 mapSquare
     mapSquare = (OpenSquare <$ char '.') <|> (Tree <$ char '#')
 
-squaresAtSlope :: Slope -> TreeMap -> [MapSquare]
-squaresAtSlope Slope { di=di, dj=dj } lines = squares (0, 0)
+numTreesOnSlope :: Slope -> TreeMap -> Int
+numTreesOnSlope Slope { di=di, dj=dj } lines = squares (0, 0)
   where
     squares (i, j) = case drop i lines of
-                       line:_ -> line !! j : squares (i + di, j + dj)
-                       _ -> []
-
-numTreesOnSlope :: Slope -> TreeMap -> Int
-numTreesOnSlope slope = length . filter isTree . squaresAtSlope slope
-  where
-    isTree Tree = True
-    isTree _ = False
+                       line:_ -> case line !! j of
+                                   Tree -> 1 + squares (i + di, j + dj)
+                                   _ -> squares (i + di, j + dj)
+                       _ -> 0
 
 printResults :: TreeMap -> PuzzleAnswerPair
 printResults lines = PuzzleAnswerPair (part1, part2)
