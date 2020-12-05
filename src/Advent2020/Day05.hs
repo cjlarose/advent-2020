235c59164f514f33a7246181327d109d82162bc7
Chris LaRose <cjlarose@gmail.com>
2020-12-04 22:26:45 -0800

    Use record to represent Seat

--- src/Advent2020/Day05.hs
+++ src/Advent2020/Day05.hs
@@ -15,18 +15,18 @@ import Advent.Input (getProblemInputAsByteString, withSuccessfulParse)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 import Advent.CommonParsers (linesOf, nonNegativeInteger)
 
-newtype Seat = Seat (Int, Int) deriving (Show, Eq, Ord)
+data Seat = Seat { row :: Int, col :: Int } deriving (Show, Eq, Ord)
 
 inputParser :: Parser [Seat]
 inputParser = linesOf seat
   where
     asInt = foldl (\acc x -> acc * 2 + x) 0
-    seat = curry Seat <$> (asInt <$> many1 fb) <*> (asInt <$> many1 lr)
+    seat = (\r c -> Seat { row=r, col=c }) <$> (asInt <$> many1 fb) <*> (asInt <$> many1 lr)
     fb = (0 <$ char 'F') <|> (1 <$ char 'B')
     lr = (0 <$ char 'L') <|> (1 <$ char 'R')
 
 seatId :: Seat -> Int
-seatId (Seat (r, c)) = r * 8 + c
+seatId seat = row seat * 8 + col seat
 
 mySeatId :: [Seat] -> Int
 mySeatId seats = mySeat
