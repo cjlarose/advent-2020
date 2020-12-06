50af9ff4c07cd10a12b5b37ba3ca32c6d9d41c03
Chris LaRose <cjlarose@gmail.com>
2020-12-05 19:19:47 -0800

    Implement Day 1 with a DP solution

--- src/Advent2020/Day01.hs
+++ src/Advent2020/Day01.hs
@@ -1,44 +1,53 @@
 module Advent2020.Day01
   ( solve
+  , subsetSums
   ) where
 
 import qualified Data.Set as Set
 import Data.Set (Set)
+import qualified Data.Map.Strict as Map
+import Data.Map (Map, (!))
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.List (foldl')
 import Control.Monad (guard)
+import Debug.Trace (traceShowId, traceShow)
 
 import Advent.Input (getProblemInputAsByteString, withSuccessfulParse)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 import Advent.CommonParsers (listOfNonNegativeIntegers)
 
-pairs :: [Int] -> [(Int, Int)]
-pairs [] = []
-pairs (x:xs) = map (\y -> (x, y)) xs ++ pairs xs
-
-productOfSpecialPair :: [Int] -> Int
-productOfSpecialPair entries = x * y
+subsetSums :: Int -> Set Int -> [Set Int]
+subsetSums k xs = subsetsForSum ! (xs, k)
   where
-    entrySet = Set.fromList entries
-    (x, y) = head $ do
-      a <- entries
-      let b = 2020 - a
-      guard $ b `Set.member` entrySet
-      pure (a, b)
+    prefixes = map (`Set.take` xs) [0..Set.size xs]
+    init = Map.fromList . map (\prefix -> ((prefix, 0), [Set.empty])) $ prefixes
+    subsetsForSum = foldl' f init [(prefix, target) | prefix <- prefixes, target <- [1..k]]
+    f :: Map (Set Int, Int) [Set Int] -> (Set Int, Int) -> Map (Set Int, Int) [Set Int]
+    f acc (prefix, target) =
+      case Set.maxView prefix of
+        Nothing -> Map.insert (prefix, target) [] acc
+        Just (x, rest) -> if x > target
+                          then Map.insert (prefix, target) (acc ! (rest, target)) acc
+                          else let l = acc ! (rest, target)
+                                   r = map (Set.insert x) $ acc ! (rest, target - x)
+                               in Map.insert (prefix, target) (l ++ r) acc
 
-productOfSpecialTriple :: [Int] -> Int
-productOfSpecialTriple entries = x * y * z
+productOfSpecialPair :: [Set Int] -> Int
+productOfSpecialPair subsets = x * y
   where
-    entrySet = Set.fromList entries
-    (x, y, z) = head $ do
-      (a, b) <- pairs entries
-      let c = 2020 - a - b
-      guard $ c `Set.member` entrySet
-      pure (a, b, c)
+    [x, y] = Set.toList . head . filter (\xs -> Set.size xs == 2) $ subsets
+
+productOfSpecialTriple :: [Set Int] -> Int
+productOfSpecialTriple subsets = x * y * z
+  where
+    [x, y, z] = Set.toList . head . filter (\xs -> Set.size xs == 3) $ subsets
 
 printResults :: [Int] -> PuzzleAnswerPair
 printResults entries = PuzzleAnswerPair (part1, part2)
   where
-    part1 = show . productOfSpecialPair $ entries
-    part2 = show . productOfSpecialTriple $ entries
+    subsets = subsetSums 2020 . Set.fromList $ entries
+    part1 = show . productOfSpecialPair $ subsets
+    part2 = show . productOfSpecialTriple $ subsets
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = withSuccessfulParse listOfNonNegativeIntegers printResults <$> getProblemInputAsByteString 1
