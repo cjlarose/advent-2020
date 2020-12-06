e7235e624c85777cb26f919c46ed7ce5fada37d1
Chris LaRose <cjlarose@gmail.com>
2020-12-05 21:19:14 -0800

    Represent group answers as sets

--- src/Advent2020/Day06.hs
+++ src/Advent2020/Day06.hs
@@ -3,6 +3,7 @@ module Advent2020.Day06
   ) where
 
 import qualified Data.Set as Set
+import Data.Set (Set)
 import Text.Parsec.ByteString (Parser)
 import Text.Parsec.Char (endOfLine, space)
 import Text.Parsec (many1, sepBy1, eof)
@@ -11,16 +12,16 @@ import Advent.Input (getProblemInputAsByteString, withSuccessfulParse)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 import Advent.CommonParsers (word)
 
-type GroupAnswers = [String]
+type GroupAnswers = Set String
 
 inputParser :: Parser [GroupAnswers]
-inputParser = sepBy1 (many1 (word <* space)) endOfLine <* eof
+inputParser = sepBy1 (Set.fromList <$> many1 (word <* space)) endOfLine <* eof
 
 numQuestionsAny :: GroupAnswers -> Int
-numQuestionsAny = Set.size . Set.unions . map Set.fromList
+numQuestionsAny = Set.size . Set.unions . Set.map Set.fromList
 
 numQuestionsAll :: GroupAnswers -> Int
-numQuestionsAll = Set.size . foldl Set.intersection (Set.fromList ['a'..'z']) . map Set.fromList
+numQuestionsAll = Set.size . foldl Set.intersection (Set.fromList ['a'..'z']) . Set.map Set.fromList
 
 printResults :: [GroupAnswers] -> PuzzleAnswerPair
 printResults groups = PuzzleAnswerPair (part1, part2)
