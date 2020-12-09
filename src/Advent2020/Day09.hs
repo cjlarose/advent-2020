f5c0859787ea550cfc1031771325b2f8aafd40f2
Chris LaRose <cjlarose@gmail.com>
2020-12-08 21:23:16 -0800

    Complete day 9 part 2

--- src/Advent2020/Day09.hs
+++ src/Advent2020/Day09.hs
@@ -2,10 +2,12 @@ module Advent2020.Day09
   ( solve
   ) where
 
-import Data.List (find)
-import Data.Maybe (isJust)
+import Data.List (find, foldl')
+import Data.Maybe (isJust, fromJust)
 import qualified Data.Set as Set
 import Data.Set (Set)
+import qualified Data.Map as Map
+import Data.Map (Map, (!))
 import qualified Data.Sequence as Seq
 import Data.Sequence (Seq, (!?), adjust')
 import Text.Parsec.ByteString (Parser)
@@ -26,11 +28,20 @@ pairs (x:xs) = map (\y -> (x, y)) xs ++ pairs xs
 constructableFromPreamble :: [Int] -> Int -> Bool
 constructableFromPreamble preamble k = isJust . find (\(a, b) -> a + b == k) . pairs $ preamble
 
+subsequenceSum :: Int -> [Int] -> Int
+subsequenceSum k xs = maximum seq + minimum seq
+  where
+    seq = go 2
+    go sz = case find ((== k) . sum) (map (\i -> take sz . drop i $ xs) [0..length xs]) of
+              Nothing -> go . succ $ sz
+              Just seq -> seq
+
 printResults :: [Int] -> PuzzleAnswerPair
 printResults ints = PuzzleAnswerPair (part1, part2)
   where
-    part1 = show . head . filter (not . snd) $ zipWith (\k prev25 -> (k, constructableFromPreamble prev25 k)) (drop 25 ints) (map (\i -> take 25 . drop i $ ints) [0..])
-    part2 = "not implemented"
+    invalid = head . map fst . filter (not . snd) $ zipWith (\k prev25 -> (k, constructableFromPreamble prev25 k)) (drop 25 ints) (map (\i -> take 25 . drop i $ ints) [0..])
+    part1 = show invalid
+    part2 = show . subsequenceSum invalid $ ints
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = withSuccessfulParse inputParser printResults <$> getProblemInputAsByteString 9
