628fd14427b1ec8936d7631d12da5c861c6413c9
Chris LaRose <cjlarose@gmail.com>
2020-12-08 22:18:47 -0800

    Fix subLists to go to the end of the list

--- src/Advent/ListUtils.hs
+++ src/Advent/ListUtils.hs
@@ -12,7 +12,7 @@ pairs (x:xs) = map (\y -> (x, y)) xs ++ pairs xs
 
 -- Returns contiguous subsequences of a list
 subLists :: [Int] -> [[Int]]
-subLists xs = go 0
+subLists xs = [] : go 1
   where
     go seqLen | seqLen > length xs = []
-              | otherwise = (map (take seqLen) . take (length xs - seqLen) . tails $ xs) ++ go (succ seqLen)
+              | otherwise = (map (take seqLen) . take (length xs - seqLen + 1) . tails $ xs) ++ go (succ seqLen)
