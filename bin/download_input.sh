c6abc810619ec339ee1ee3e0d1b402efb113eedc
Chris LaRose <cjlarose@gmail.com>
2020-12-10 17:44:05 -0800

    Add check to ensure session cookie is set

--- bin/download_input.sh
+++ bin/download_input.sh
@@ -6,6 +6,12 @@ then
   exit 1
 fi
 
+if [[ -z $ADVENT_SESSION_COOKIE ]]
+then
+  echo 'Missing environment variable ADVENT_SESSION_COOKIE' >&2
+  exit 1
+fi
+
 problem_number=$1
 url='https://adventofcode.com/2020/day/'$problem_number'/input'
 output_filename=inputs/"$problem_number".txt
