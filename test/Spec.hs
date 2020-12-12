9ff4fce6a7ef91afd18695bf0006d630997e8f4e
Chris LaRose <cjlarose@gmail.com>
2020-12-12 12:04:58 -0800

    Split tests for differents problem parts

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,23 +8,27 @@ import Test.Tasty.Golden (goldenVsString, findByExtension)
 import Advent2020.Solve (solverForProblem)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 
-resultToBS :: Either String PuzzleAnswerPair -> LBS.ByteString
-resultToBS (Left err) = runPut $ putStringUtf8 err >> putCharUtf8 '\n'
-resultToBS (Right (PuzzleAnswerPair (part1, part2))) = runPut $ do
-                                                         putStringUtf8 part1
-                                                         putCharUtf8 '\n'
-                                                         putStringUtf8 part2
-                                                         putCharUtf8 '\n'
+resultToBS :: ((String, String) -> String) -> Either String PuzzleAnswerPair -> LBS.ByteString
+resultToBS _ (Left err) = runPut $ putStringUtf8 err >> putCharUtf8 '\n'
+resultToBS f (Right (PuzzleAnswerPair answers)) = runPut $ do
+                                                             putStringUtf8 . f $ answers
+                                                             putCharUtf8 '\n'
 
 main :: IO ()
 main = defaultMain =<< goldenTests
 
-goldenTestForProblem :: Int -> TestTree
-goldenTestForProblem n = goldenVsString testName outputFile action
+goldenTestForProblemPart :: Int -> Int -> TestTree
+goldenTestForProblemPart n p = goldenVsString testName outputFile action
   where
-    testName = "Day " ++ show n
-    outputFile = "outputs" </> show n <.> "txt"
-    action = resultToBS <$> solverForProblem n
+    testName = "Part " ++ show p
+    outputFile = "outputs" </> "day_" ++ show n </> "part_" ++ show p <.> "txt"
+    action = resultToBS (if p == 1 then fst else snd) <$> solverForProblem n
+
+goldenTestForProblem :: Int -> TestTree
+goldenTestForProblem n = testGroup groupName tests
+  where
+    groupName = "Day " ++ show n
+    tests = [goldenTestForProblemPart n 1, goldenTestForProblemPart n 2]
 
 goldenTests :: IO TestTree
 goldenTests = do
