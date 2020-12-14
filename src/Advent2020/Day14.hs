cac4a29c7ac6f7aacfce7ce9a556a4db3ff2c162
Chris LaRose <cjlarose@gmail.com>
2020-12-13 23:07:33 -0800

    Inline parseVersion1Mask

--- src/Advent2020/Day14.hs
+++ src/Advent2020/Day14.hs
@@ -17,7 +17,6 @@ import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 import Advent.CommonParsers (linesOf, natural, word, integerWithOptionalLeadingSign)
 
 newtype Address = Address Natural deriving Show
-data MaskV1 = MaskV1 { getClearingMask :: Integer, getSettingMask :: Integer } deriving Show
 newtype Mask = Mask String deriving Show
 data Instruction = SetMask Mask | Write Address Integer deriving Show
 
@@ -28,25 +27,22 @@ inputParser = linesOf instruction
     maskAssignment = SetMask <$> (string "mask = " *> (Mask <$> word))
     writeToAddress = Write <$> (string "mem[" *> (Address <$> natural)) <*> (string "] = " *> integerWithOptionalLeadingSign)
 
--- | Constructs a version 1 mask given a mask in its string form. A verison 1
--- mask is composed to two parts: a clearing mask and setting mask. The
--- clearing mask has a 0 in every bit position we want to clear, 1s elsewhere.
--- The setting mask has a 1 in every position we want to set to 1, 0s
--- elsewhere.
-parseVersion1Mask :: Mask -> MaskV1
-parseVersion1Mask (Mask xs) = let (c, s) = foldl' f (0, 0) xs in MaskV1 { getClearingMask=c, getSettingMask=s }
+-- | Applies a version 1 mask given a mask in its string form. A verison 1 mask
+-- is composed to two parts: a clearing mask and setting mask. The clearing
+-- mask has a 0 in every bit position we want to clear, 1s elsewhere. The
+-- setting mask has a 1 in every position we want to set to 1, 0s elsewhere.
+applyMaskV1 :: Mask -> Integer -> Integer
+applyMaskV1 (Mask mask) = setBits . clearBits
   where
+    (c, s) = foldl' f (0, 0) mask
+
     f :: (Integer, Integer) -> Char -> (Integer, Integer)
     f (clearMask, setMask) '0' = (shiftL clearMask 1, shiftL setMask 1)
     f (clearMask, setMask) '1' = (shiftL clearMask 1 .|. 1, shiftL setMask 1 .|. 1)
     f (clearMask, setMask) 'X' = (shiftL clearMask 1 .|. 1, shiftL setMask 1)
 
-applyMaskV1 :: Mask -> Integer -> Integer
-applyMaskV1 mask = setBits . clearBits
-  where
-    maskV1 = parseVersion1Mask mask
-    clearBits = (.&.) (getClearingMask maskV1)
-    setBits = (.|.) (getSettingMask maskV1)
+    clearBits = (.&.) c
+    setBits = (.|.) s
 
 applyMaskV2 :: Mask -> Integer -> [Integer]
 applyMaskV2 (Mask mask) val = do
