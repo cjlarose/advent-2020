742f75607a937cb7f13afee85d633c2e311a8a17
Chris LaRose <cjlarose@gmail.com>
2020-12-14 20:39:07 -0800

    Remove unused <<|

--- src/Advent/BitUtils.hs
+++ src/Advent/BitUtils.hs
@@ -1,13 +1,8 @@
 module Advent.BitUtils
-  ((<<|)
-  , fromBits
+  ( fromBits
   ) where
 
-import Data.Bits (Bits, (.|.), shiftL, zeroBits, setBit)
-
--- | a <<| b shfits a by one bit position, then applies a bitwise OR with b
-(<<|) :: Bits a => a -> a -> a
-(<<|) x y = shiftL x 1 .|. y
+import Data.Bits (Bits, shiftL, zeroBits, setBit)
 
 fromBits :: (Foldable t, Bits b) => t Bool -> b
 fromBits = foldl f zeroBits
