1178b35d74559ae52ddadd0cc5ceff90e026805f
Chris LaRose <cjlarose@gmail.com>
2020-12-14 20:56:45 -0800

    Use foldl' in fromBits

--- src/Advent/BitUtils.hs
+++ src/Advent/BitUtils.hs
@@ -2,10 +2,11 @@ module Advent.BitUtils
   ( fromBits
   ) where
 
+import Data.Foldable (foldl')
 import Data.Bits (Bits, shiftL, zeroBits, setBit)
 
 fromBits :: (Foldable t, Bits b) => t Bool -> b
-fromBits = foldl f zeroBits
+fromBits = foldl' f zeroBits
   where
     f acc True = setBit (shiftL acc 1) 0
     f acc False = shiftL acc 1
