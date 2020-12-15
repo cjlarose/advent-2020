0549e6f2eae174455e05bbeb682fc7713b2cb17a
Chris LaRose <cjlarose@gmail.com>
2020-12-14 18:31:11 -0800

    Add fromBits util

--- src/Advent/BitUtils.hs
+++ src/Advent/BitUtils.hs
@@ -1,9 +1,16 @@
 module Advent.BitUtils
   ((<<|)
+  , fromBits
   ) where
 
-import Data.Bits (Bits, (.|.), shiftL)
+import Data.Bits (Bits, (.|.), shiftL, zeroBits, setBit)
 
 -- | a <<| b shfits a by one bit position, then applies a bitwise OR with b
 (<<|) :: Bits a => a -> a -> a
 (<<|) x y = shiftL x 1 .|. y
+
+fromBits :: (Foldable t, Bits b) => t Bool -> b
+fromBits = foldl f zeroBits
+  where
+    f acc True = setBit (shiftL acc 1) 0
+    f acc False = shiftL acc 1
