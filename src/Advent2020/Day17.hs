ba52d07f258957ab8e6acae3c411e8b190bdfa6d
Chris LaRose <cjlarose@gmail.com>
2020-12-16 22:00:05 -0800

    Add HigherDimensionCoord typeclass to make simulateCycles generic

--- src/Advent2020/Day17.hs
+++ src/Advent2020/Day17.hs
@@ -17,11 +17,33 @@ import Advent.CommonParsers (linesOf)
 type Coord2D = (Int, Int)
 type Pocket2Dimension = Set Coord2D
 
-type Coord3D = (Int, Int, Int)
-type Pocket3Dimension = Set Coord3D
+newtype Coord3D = Coord3D (Int, Int, Int) deriving (Eq, Ord)
+newtype Coord4D = Coord4D (Int, Int, Int, Int) deriving (Eq, Ord)
 
-type Coord4D = (Int, Int, Int, Int)
-type Pocket4Dimension = Set Coord4D
+class Ord a => HigherDimensionCoord a where
+  neighbors :: a -> Set a
+  from2d :: Coord2D -> a
+
+instance HigherDimensionCoord Coord3D where
+  neighbors coord@(Coord3D (x, y, z)) = Set.fromList $ do
+    newX <- [x-1..x+1]
+    newY <- [y-1..y+1]
+    newZ <- [z-1..z+1]
+    let neighbor = Coord3D (newX, newY, newZ)
+    guard $ neighbor /= coord
+    pure neighbor
+  from2d (x, y) = Coord3D (x, y, 0)
+
+instance HigherDimensionCoord Coord4D where
+  neighbors coord@(Coord4D (x, y, z, w)) = Set.fromList $ do
+    newX <- [x-1..x+1]
+    newY <- [y-1..y+1]
+    newZ <- [z-1..z+1]
+    newW <- [w-1..w+1]
+    let neighbor = Coord4D (newX, newY, newZ, newW)
+    guard $ neighbor /= coord
+    pure neighbor
+  from2d (x, y) = Coord4D (x, y, 0, 0)
 
 inputParser :: Parser Pocket2Dimension
 inputParser = toSet <$> linesOf row
@@ -35,16 +57,7 @@ inputParser = toSet <$> linesOf row
       pure . Just $ (line - 1, col - 1)
     toSet xs = Set.fromList . map (\(i, j) -> (j, i)) . catMaybes . concat $ xs
 
-neighbors :: Coord3D -> Set Coord3D
-neighbors coord@(x, y, z) = Set.fromList $ do
-  newX <- [x-1..x+1]
-  newY <- [y-1..y+1]
-  newZ <- [z-1..z+1]
-  let neighbor = (newX, newY, newZ)
-  guard $ neighbor /= coord
-  pure neighbor
-
-simulateCycles :: Int -> Pocket3Dimension -> Pocket3Dimension
+simulateCycles :: HigherDimensionCoord a => Int -> Set a -> Set a
 simulateCycles 0 active = active
 simulateCycles rounds active = simulateCycles (pred rounds) newActive
   where
@@ -57,38 +70,11 @@ simulateCycles rounds active = simulateCycles (pred rounds) newActive
 
 activeCubes = Set.size
 
-extendIntoThirdDimension :: Coord2D -> Coord3D
-extendIntoThirdDimension (x, y) = (x, y, 0)
-
-extendIntoFourthDimension :: Coord2D -> Coord4D
-extendIntoFourthDimension (x, y) = (x, y, 0, 0)
-
-neighbors4d :: Coord4D -> Set Coord4D
-neighbors4d coord@(x, y, z, w) = Set.fromList $ do
-  newX <- [x-1..x+1]
-  newY <- [y-1..y+1]
-  newZ <- [z-1..z+1]
-  newW <- [w-1..w+1]
-  let neighbor = (newX, newY, newZ, newW)
-  guard $ neighbor /= coord
-  pure neighbor
-
-simulateCycles4d :: Int -> Pocket4Dimension -> Pocket4Dimension
-simulateCycles4d 0 active = active
-simulateCycles4d rounds active = simulateCycles4d (pred rounds) newActive
-  where
-    activeNeighbors = Set.intersection active . neighbors4d
-    candidates = Set.unions . Set.map neighbors4d $ active
-    inactive = candidates \\ active
-    newlyActive = Set.filter ((==) 3 . Set.size . activeNeighbors) inactive
-    remainingActive = Set.filter ((\k -> k == 2 || k == 3) . Set.size . activeNeighbors) active
-    newActive = Set.union newlyActive remainingActive
-
 printResults :: Pocket2Dimension -> PuzzleAnswerPair
 printResults initialState = PuzzleAnswerPair (part1, part2)
   where
-    part1 = show . activeCubes . simulateCycles 6 . Set.map extendIntoThirdDimension $ initialState
-    part2 = show . activeCubes . simulateCycles4d 6 . Set.map extendIntoFourthDimension $ initialState
+    part1 = show . activeCubes . simulateCycles 6 . Set.map (from2d :: Coord2D -> Coord3D) $ initialState
+    part2 = show . activeCubes . simulateCycles 6 . Set.map (from2d :: Coord2D -> Coord4D) $ initialState
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = withSuccessfulParse inputParser printResults <$> getProblemInputAsByteString 17
