5aa3f3bdc0b3b264cedbdcec1d0666558e1dd42f
Chris LaRose <cjlarose@gmail.com>
2020-12-16 21:32:02 -0800

    Complete day 17, part 2

--- src/Advent2020/Day17.hs
+++ src/Advent2020/Day17.hs
@@ -20,6 +20,9 @@ type Pocket2Dimension = Set Coord2D
 type Coord3D = (Int, Int, Int)
 type Pocket3Dimension = Set Coord3D
 
+type Coord4D = (Int, Int, Int, Int)
+type Pocket4Dimension = Set Coord4D
+
 inputParser :: Parser Pocket2Dimension
 inputParser = toSet <$> linesOf row
   where
@@ -52,17 +55,40 @@ simulateCycles rounds active = simulateCycles (pred rounds) newActive
     remainingActive = Set.filter ((\k -> k == 2 || k == 3) . Set.size . activeNeighbors) active
     newActive = Set.union newlyActive remainingActive
 
-activeCubes :: Pocket3Dimension -> Int
 activeCubes = Set.size
 
 extendIntoThirdDimension :: Coord2D -> Coord3D
 extendIntoThirdDimension (x, y) = (x, y, 0)
 
+extendIntoFourthDimension :: Coord2D -> Coord4D
+extendIntoFourthDimension (x, y) = (x, y, 0, 0)
+
+neighbors4d :: Coord4D -> Set Coord4D
+neighbors4d coord@(x, y, z, w) = Set.fromList $ do
+  newX <- [x-1..x+1]
+  newY <- [y-1..y+1]
+  newZ <- [z-1..z+1]
+  newW <- [w-1..w+1]
+  let neighbor = (newX, newY, newZ, newW)
+  guard $ neighbor /= coord
+  pure neighbor
+
+simulateCycles4d :: Int -> Pocket4Dimension -> Pocket4Dimension
+simulateCycles4d 0 active = active
+simulateCycles4d rounds active = simulateCycles4d (pred rounds) newActive
+  where
+    activeNeighbors = Set.intersection active . neighbors4d
+    candidates = Set.unions . Set.map neighbors4d $ active
+    inactive = candidates \\ active
+    newlyActive = Set.filter ((==) 3 . Set.size . activeNeighbors) inactive
+    remainingActive = Set.filter ((\k -> k == 2 || k == 3) . Set.size . activeNeighbors) active
+    newActive = Set.union newlyActive remainingActive
+
 printResults :: Pocket2Dimension -> PuzzleAnswerPair
 printResults initialState = PuzzleAnswerPair (part1, part2)
   where
     part1 = show . activeCubes . simulateCycles 6 . Set.map extendIntoThirdDimension $ initialState
-    part2 = "not implemented"
+    part2 = show . activeCubes . simulateCycles4d 6 . Set.map extendIntoFourthDimension $ initialState
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = withSuccessfulParse inputParser printResults <$> getProblemInputAsByteString 17
