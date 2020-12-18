a7d08f5c68f3abc0788ae2eb867eb3aab41df782
Chris LaRose <cjlarose@gmail.com>
2020-12-17 20:23:40 -0800

    Store index + 1, so we leave uninitialized parts of the vector with 0

--- src/Advent2020/Day15.hs
+++ src/Advent2020/Day15.hs
@@ -22,18 +22,13 @@ inputParser = sepBy1 integerWithOptionalLeadingSign (char ',') <* endOfLine <* e
 writeSafely :: (PrimMonad m) => MVector (PrimState m) Int -> Int -> Int -> m (MVector (PrimState m) Int)
 writeSafely vector i val = do
   newVec <- iterateUntilM (\v -> i < V.length v)
-              (\v -> do
-                let oldLength = V.length v
-                new <- V.grow v oldLength
-                let newLength = V.length new
-                mapM_ (\i -> V.write new i (-1)) [oldLength..newLength-1]
-                pure new)
+              (\v -> V.grow v (V.length v))
               vector
   V.write newVec i val
   pure newVec
 
 recordLastSeenIndex :: (PrimMonad m) => MVector (PrimState m) Int -> Int -> Int -> m (MVector (PrimState m) Int)
-recordLastSeenIndex vector element lastIndex = writeSafely vector element lastIndex
+recordLastSeenIndex vector element lastIndex = writeSafely vector element (lastIndex + 1)
 
 totalRead :: (PrimMonad m, Unbox a) => MVector (PrimState m) a -> Int -> m (Maybe a)
 totalRead vector i =
@@ -45,14 +40,14 @@ lastIndexOf :: (PrimMonad m) => MVector (PrimState m) Int -> Int -> m (Maybe Int
 lastIndexOf vector i = parseValue <$> totalRead vector i
   where
     parseValue Nothing = Nothing
-    parseValue (Just (-1)) = Nothing
-    parseValue (Just index) = Just index
+    parseValue (Just 0) = Nothing
+    parseValue (Just index) = Just $ index - 1
 
 spokenAt :: Int -> [Int] -> Int
 spokenAt k inits = runST $ do
   let initialElements :: [(Int, Int)]
       initialElements = zip (init inits) [0..]
-  start <- V.replicate 8 (-1)
+  start <- V.new 8
   mostRecentIndex <- foldM (\vec (element, index) -> recordLastSeenIndex vec element index) start initialElements
   let f (i, prev, vec) = do oldVal <- lastIndexOf vec prev
                             newVec <- recordLastSeenIndex vec prev (i - 1)
