a249aa08f12bcd9d16645018a9aeaa3b0837b7a4
Chris LaRose <cjlarose@gmail.com>
2020-12-17 22:33:28 -0800

    Complete Day 18 part 2

--- src/Advent2020/Day18.hs
+++ src/Advent2020/Day18.hs
@@ -3,9 +3,9 @@ module Advent2020.Day18
   ) where
 
 import Text.Parsec.ByteString (Parser)
-import Text.Parsec (char, (<|>), between, chainl1)
+import Text.Parsec (try, char, (<|>), between, chainl1, parse)
 
-import Advent.Input (getProblemInputAsByteString, withSuccessfulParse)
+import Advent.Input (getProblemInputAsByteString)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 import Advent.CommonParsers (linesOf, integerWithOptionalLeadingSign)
 
@@ -24,16 +24,31 @@ inputParser = linesOf binaryExpression
     operator = ((`BinaryExpression` Plus) <$ char '+') <|> ((`BinaryExpression` Times) <$ char '*')
     literalExpression = Literal <$> integerWithOptionalLeadingSign
 
+inputParser' :: Parser [Expression]
+inputParser' = linesOf binaryExpression
+  where
+    binaryExpression = chainl1 factor (try (char ' ' *> mulOp <* char ' '))
+    factor = chainl1 term (try (char ' ' *> addOp <* char ' '))
+    term = literalExpression <|> parenthesizedExpression
+    parenthesizedExpression = between (char '(') (char ')') binaryExpression
+    mulOp = (`BinaryExpression` Times) <$ char '*'
+    addOp = (`BinaryExpression` Plus) <$ char '+'
+    literalExpression = Literal <$> integerWithOptionalLeadingSign
+
 evaluate :: Expression -> Integer
 evaluate (Literal x) = x
 evaluate (BinaryExpression lhs Plus rhs) = evaluate lhs + evaluate rhs
 evaluate (BinaryExpression lhs Times rhs) = evaluate lhs * evaluate rhs
 
-printResults :: [Expression] -> PuzzleAnswerPair
-printResults expressions = PuzzleAnswerPair (part1, part2)
-  where
-    part1 = show . sum . map evaluate $ expressions
-    part2 = "not implemented"
-
 solve :: IO (Either String PuzzleAnswerPair)
-solve = withSuccessfulParse inputParser printResults <$> getProblemInputAsByteString 18
+solve = do
+  input <- getProblemInputAsByteString 18
+  case parse inputParser "" input of
+    Left err -> pure . Left . show $ err
+    Right expressions -> do
+      let part1 = show . sum . map evaluate $ expressions
+      case parse inputParser' "" input of
+        Left err -> pure . Left . show $ err
+        Right expressions' -> do
+          let part2 = show . sum . map evaluate $ expressions'
+          pure . Right . PuzzleAnswerPair $ (part1, part2)
