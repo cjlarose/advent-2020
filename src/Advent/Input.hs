56b3d757426ce50a5dabf8c99092f96c811981d5
Chris LaRose <cjlarose@gmail.com>
2020-12-18 17:28:53 -0800

    Add helper for parsing with megaparsec

--- src/Advent/Input.hs
+++ src/Advent/Input.hs
@@ -1,12 +1,19 @@
 module Advent.Input
   ( getProblemInputAsString
   , getProblemInputAsByteString
+  , getProblemInputAsText
   , withSuccessfulParse
+  , withSuccessfulParse'
   ) where
 
 import qualified Data.ByteString as B
 import qualified Text.Parsec (parse)
 import Text.Parsec.ByteString (Parser)
+import Text.Megaparsec (Parsec)
+import qualified Text.Megaparsec as Megaparsec
+import Data.Text (Text)
+import Data.Void (Void)
+import Data.Text.Encoding (decodeUtf8)
 
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair)
 
@@ -19,5 +26,11 @@ getProblemInputAsString = readFile . path
 getProblemInputAsByteString :: Int -> IO B.ByteString
 getProblemInputAsByteString = B.readFile . path
 
+getProblemInputAsText :: Int -> IO Text
+getProblemInputAsText problemNumber = decodeUtf8 <$> getProblemInputAsByteString problemNumber
+
 withSuccessfulParse :: Parser a -> (a -> PuzzleAnswerPair) -> B.ByteString -> Either String PuzzleAnswerPair
 withSuccessfulParse p f x = either (Left . show) (Right . f) $ Text.Parsec.parse p "" x
+
+withSuccessfulParse' :: Parsec Void Text a -> (a -> PuzzleAnswerPair) -> Text -> Either String PuzzleAnswerPair
+withSuccessfulParse' p f x = either (Left . show) (Right . f) $ Megaparsec.parse p "" x
