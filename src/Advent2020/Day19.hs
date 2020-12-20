fbfcaa3bb73c67c382110024f14f54b81ac9fbae
Chris LaRose <cjlarose@gmail.com>
2020-12-19 19:12:41 -0800

    Try to preserve text content

--- src/Advent2020/Day19.hs
+++ src/Advent2020/Day19.hs
@@ -6,10 +6,8 @@ module Advent2020.Day19
 
 import qualified Data.IntMap as IntMap
 import Data.IntMap (IntMap, (!))
-import qualified Data.Text as Text
-import Data.Text (Text)
 import Text.Megaparsec (eof, (<|>), try, some, skipMany, lookAhead)
-import Text.Megaparsec.Char (eol, char, string, lowerChar)
+import Text.Megaparsec.Char (eol, char, lowerChar)
 import Text.Megaparsec.Char.Lexer (decimal)
 import Control.Applicative.Combinators (between, count, choice, sepBy1)
 
@@ -17,7 +15,7 @@ import Advent.Input (getProblemInputAsText)
 import Advent.Parse (Parser, parse, symbol, word, linesOf)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 
-data MessageValidity = Valid | Invalid deriving Show
+data MessageValidity = Valid String | Invalid String deriving Show
 
 data Replacement = Terminal Char | Alternatives [[Int]] deriving Show
 newtype Productions = Productions (IntMap Replacement) deriving Show
@@ -33,16 +31,16 @@ productionsAST = Productions . foldr (\(id, r) acc -> IntMap.insert id r acc) In
     literalRule = Terminal <$> token (between (char '"') (char '"') lowerChar)
     choiceRule = Alternatives <$> sepBy1 (some (token decimal)) (symbol "|")
 
-parserForDeterministicCFG :: Productions -> (IntMap (Parser Text), Parser MessageValidity)
+parserForDeterministicCFG :: Productions -> (IntMap (Parser String), Parser MessageValidity)
 parserForDeterministicCFG (Productions rules) = (ruleMap', startRule)
   where
     ruleMap = IntMap.map toParser rules
-    toParser :: Replacement -> IntMap (Parser Text) -> Parser Text
-    toParser (Terminal c) _ = string . Text.singleton $ c
+    toParser :: Replacement -> IntMap (Parser String) -> Parser String
+    toParser (Terminal c) _ = pure <$> char c
     toParser (Alternatives options) m = choice . map (try . parserForNonterminalSequence m) $ options
     parserForNonterminalSequence m = foldr (\x acc -> (m ! x) <* acc) (pure "")
     ruleMap' = IntMap.map (\v -> v ruleMap') ruleMap
-    startRule = try (Valid <$ ((ruleMap' ! 0) <* eol)) <|> (Invalid <$ (word <* eol))
+    startRule = try (Valid <$> ((ruleMap' ! 0) <* eol)) <|> (Invalid <$> (word <* eol))
 
 inputParser :: Parser ([MessageValidity], [MessageValidity])
 inputParser = do
@@ -61,19 +59,19 @@ inputParser = do
         count k (ruleMap ! 42)
         rest <- some (ruleMap ! 31)
         if length rest < k
-        then eol
+        then head rest <$ eol
         else fail "no parse"
       matchesRule0' = do
         maxK <- length <$> lookAhead word
         let p = choice . map (try . withPrefixSize) $ [maxK,maxK-1..1]
-        try (Valid <$ p) <|> (Invalid <$ (word <* eol))
+        try (Valid <$> p) <|> (Invalid <$> (word <* eol))
   ((,) <$> lookAhead (some matchesRule0) <*> some matchesRule0') <* eof
 
 printResults :: ([MessageValidity], [MessageValidity]) -> PuzzleAnswerPair
 printResults (messagesBeforeChange, messagesAfterChange) = PuzzleAnswerPair (part1, part2)
   where
-    isValid Valid = True
-    isValid Invalid = False
+    isValid (Valid _) = True
+    isValid (Invalid _) = False
     part1 = show . length . filter isValid $ messagesBeforeChange
     part2 = show . length . filter isValid $ messagesAfterChange
 
