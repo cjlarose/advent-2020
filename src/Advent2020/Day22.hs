6aa5a0e220e20d72fd65998638981d9fe39fbc1c
Chris LaRose <cjlarose@gmail.com>
2020-12-22 22:16:38 -0800

    Complete day 22 part 2

--- src/Advent2020/Day22.hs
+++ src/Advent2020/Day22.hs
@@ -6,6 +6,8 @@ module Advent2020.Day22
 
 import Data.Sequence (Seq((:<|)), (|>))
 import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as Set
 import Data.Foldable (toList)
 import Text.Megaparsec (some, eof)
 import Text.Megaparsec.Char.Lexer (decimal)
@@ -17,6 +19,7 @@ import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 newtype Card = Card Int deriving (Show, Eq, Ord)
 type Deck = Seq Card
 type CombatDecks = (Deck, Deck)
+data Player = P1 | P2
 
 inputParser :: Parser CombatDecks
 inputParser = (,) <$> deck "1" <*> deck "2" <* eof
@@ -36,6 +39,26 @@ playGame = go
         then go (restP1 |> topP1 |> topP2, restP2)
         else go (restP1, restP2 |> topP2 |> topP1)
 
+playGameRecursive :: CombatDecks -> Deck
+playGameRecursive decks = winningDeck
+  where
+    playRound :: Set CombatDecks -> CombatDecks -> (Player, Deck)
+    playRound _ (Seq.Empty, p2Deck) = (P2, p2Deck)
+    playRound _ (p1Deck, Seq.Empty) = (P1, p1Deck)
+    playRound visited decks@(p1Deck,_)
+      | decks `Set.member` visited = (P1, p1Deck)
+      | otherwise = let (topP1 :<| restP1, topP2 :<| restP2) = decks
+                        valueOf (Card x) = x
+                        canRecurse = Seq.length restP1 >= valueOf topP1 && Seq.length restP2 >= valueOf topP2
+                        newVisited = Set.insert decks visited
+                        winner | canRecurse = fst . playRound Set.empty $ (Seq.take (valueOf topP1) restP1, Seq.take (valueOf topP2) restP2)
+                               | topP1 > topP2 = P1
+                               | otherwise = P2
+                    in case winner of
+                        P1 -> playRound newVisited (restP1 |> topP1 |> topP2, restP2)
+                        P2 -> playRound newVisited (restP1, restP2 |> topP2 |> topP1)
+    (_, winningDeck) = playRound Set.empty decks
+
 getDeckScore :: Deck -> Int
 getDeckScore deck = sum . zipWith (*) [length deck, length deck - 1..0] . map (\(Card x) -> x) . toList $ deck
 
@@ -43,7 +66,7 @@ printResults :: CombatDecks -> PuzzleAnswerPair
 printResults decks = PuzzleAnswerPair (part1, part2)
   where
     part1 = show . getDeckScore . playGame $ decks
-    part2 = "not implemented"
+    part2 = show . getDeckScore . playGameRecursive $ decks
 
 solve :: IO (Either String PuzzleAnswerPair)
 solve = parse inputParser printResults <$> getProblemInputAsText 22
