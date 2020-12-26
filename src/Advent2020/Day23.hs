b79ec281ceb260f888aa79e07f888cc94a38ef97
Chris LaRose <cjlarose@gmail.com>
2020-12-26 15:03:22 -0800

    Use map representation for cup circle, complete part 2

--- src/Advent2020/Day23.hs
+++ src/Advent2020/Day23.hs
@@ -4,12 +4,10 @@ module Advent2020.Day23
   ( solve
   ) where
 
-import qualified Data.List as List
 import Data.Char (digitToInt)
-import Data.Sequence (Seq((:<|)), (<|), (|>), (><))
-import qualified Data.Sequence as Seq
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map, (!))
 import qualified Data.Set as Set
-import Data.Foldable (toList)
 import Text.Megaparsec (some, eof)
 import Text.Megaparsec.Char (digitChar)
 
@@ -18,50 +16,78 @@ import Advent.Parse (Parser, parse, token)
 import Advent.PuzzleAnswerPair (PuzzleAnswerPair(..))
 
 newtype Cup = Cup Int deriving (Show, Eq, Ord)
-type CupCircle = Seq Cup
+data CupCircle = CupCircle { getFocus :: Cup
+                           , getNext :: Map Cup Cup
+                           } deriving Show
+
+clockwiseCups :: CupCircle -> [Cup]
+clockwiseCups CupCircle{getFocus=focus,getNext=nextMap} = focus : (takeWhile (/= focus) . drop 1 . iterate findNext $ focus)
+  where
+    findNext cup = nextMap ! cup
+
+setNextCup :: Cup -> Cup -> CupCircle -> CupCircle
+setNextCup from to circle@CupCircle{getNext=nextMap} = circle{getNext=Map.insert from to nextMap}
+
+getNextCup :: Cup -> CupCircle -> Cup
+getNextCup cup circle = getNext circle ! cup
+
+rotateRight :: CupCircle -> CupCircle
+rotateRight circle = let newFocus = getNextCup (getFocus circle) circle
+                     in circle{getFocus=newFocus}
+
+cupCirclefromList :: [Cup] -> CupCircle
+cupCirclefromList xs = CupCircle focus nextMap
+  where
+    focus = head xs
+    nexts = zip xs (tail xs ++ [head xs])
+    nextMap = foldr (\(from, to) acc -> Map.insert from to acc) Map.empty nexts
 
 inputParser :: Parser CupCircle
-inputParser = Seq.fromList <$> some cup <* eof
+inputParser = cupCirclefromList <$> some cup <* eof
   where
     cup = Cup . digitToInt <$> token digitChar
 
 simulateRounds :: Int -> CupCircle -> CupCircle
 simulateRounds k initialCircle = go k initialCircle
   where
-    sortedDescCups = reverse . List.sort . toList $ initialCircle
-    destCupForCurrent x exclude =
-      let candidates = filter (`Set.notMember` exclude) sortedDescCups
-      in head $ dropWhile (>= x) candidates ++ candidates
-    rotateLeft (front :<| rest) = rest |> front
+    maxC = maxCup initialCircle
+    destCupForCurrent (Cup x) exclude =
+      let target = if x == 1 then maxC else Cup (x - 1)
+      in if target `Set.member` exclude then destCupForCurrent target exclude else target
     go 0 circle = circle
-    go n (current :<| rest) =
-      let (removed, afterRemoved) = Seq.splitAt 3 rest
-          with3Removed = current <| afterRemoved
-          destCup = destCupForCurrent current (Set.fromList . toList $ removed)
-          (beforeDest, dest :<| afterDest) = Seq.spanl (destCup /=) with3Removed
-          with3AddedBack = (beforeDest |> dest) >< removed >< afterDest
-          newCircle = rotateLeft with3AddedBack
+    go n circle =
+      let removed@[a,_,c] = take 3 . drop 1 . clockwiseCups $ circle
+          with3Removed = setNextCup (getFocus circle) (getNextCup c circle) circle
+          destCup = destCupForCurrent (getFocus circle) . Set.fromList $ removed
+          nextOfDest = getNextCup destCup with3Removed
+          with3AddedBack = setNextCup c nextOfDest . setNextCup destCup a $ with3Removed
+          newCircle = rotateRight with3AddedBack
       in go (n - 1) newCircle
 
 bringCupToFront :: Cup -> CupCircle -> CupCircle
-bringCupToFront cup circle = after >< before
-  where
-    (before, after) = Seq.spanl (cup /=) circle
+bringCupToFront cup circle = circle{getFocus=cup}
 
 cupsAfterLabel1 :: CupCircle -> String
-cupsAfterLabel1 circle = concatMap (show . (\(Cup x) -> x)) . toList $ rest
+cupsAfterLabel1 circle = concatMap (show . (\(Cup x) -> x)) rest
   where
-    (_ :<| rest) = bringCupToFront (Cup 1) circle
+    rest = tail . clockwiseCups . bringCupToFront (Cup 1) $ circle
+
+maxCup :: CupCircle -> Cup
+maxCup = maximum . clockwiseCups
 
 extendToCupLabelled1Million :: CupCircle -> CupCircle
-extendToCupLabelled1Million circle = circle >< (Seq.fromList . map Cup $ [maxValue + 1..1000000])
+extendToCupLabelled1Million circle = newCircle
   where
-    (Cup maxValue) = maximum circle
+    (Cup maxValue) = maxCup circle
+    newCups = map Cup [maxValue + 1..1000000]
+    lastCupClockwise = last . clockwiseCups $ circle
+    nexts = zip (lastCupClockwise : newCups) (newCups ++ [getFocus circle])
+    newCircle = foldr (\(from, to) acc -> setNextCup from to acc) circle nexts
 
 productOfTwoCupsAfter1 :: CupCircle -> Int
 productOfTwoCupsAfter1 circle = a * b
   where
-    (_ :<| (Cup a) :<| (Cup b) :<| _) = bringCupToFront (Cup 1) circle
+    (_ : (Cup a) : (Cup b) : _) = clockwiseCups . bringCupToFront (Cup 1) $ circle
 
 printResults :: CupCircle -> PuzzleAnswerPair
 printResults circle = PuzzleAnswerPair (part1, part2)
